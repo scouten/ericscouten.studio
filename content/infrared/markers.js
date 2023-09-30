1f5119e8f148ea8702a3cdbe16d03f739bad9d65
Eric Scouten <git@scouten.me>
2023-09-30 10:00:20 -0700

    Seeing Red and Loving It: Mirrorless vs DSLR for Infrared

---
+++ content/infrared/markers.js
@@ -0,0 +1,15 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-2245-002", -12.577625, -38.003087),
+        addGpxMarker(map, "es-2245-018a", -12.577740, -38.003240),
+        addGpxMarker(map, "es-2276-051", -34.471432, -57.852000),
+        addGpxMarker(map, "es-2681-023", 54.061024, 9.556533),
+        addGpxMarker(map, "es-2684-010", 53.281655, 12.105390),
+        addGpxMarker(map, "es-2718-031", 40.368961, -120.573626),
+        addGpxMarker(map, "es-2722-004", 34.120433, -114.510115),
+        addGpxMarker(map, "es-2722-010", 33.819130, -113.547204),
+        addGpxMarker(map, "es-2723-022", 32.210907, -110.972412),
+        addGpxMarker(map, "es-2723-046", 32.225028, -110.973183),
+        addGpxMarker(map, "es-2730-075", 35.683252, -119.330582),
+    ]
+}
